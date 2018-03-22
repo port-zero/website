2b8e7564248e8e1545a4fcacd5887ff8d57adbc1
vidister <git@vidister.de>
2018-03-22 15:53:15 +0100

    rollback build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/sh
 set -e
 hugo
-scp -r public/* root@web01.port-ze.ro:/srv/www/port-zero.com/html
+scp -r public/* root@web-temp.port-zero.com:/var/www/port-zero.com/html
+ssh root@web-temp.port-zero.com systemctl restart caddy
