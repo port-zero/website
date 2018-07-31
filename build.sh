76e578131cb1ae9a55c21a84269bda41710802c5
hellerve <veit@veitheller.de>
2018-07-31 18:49:28 +0200

    build: conditional configuration

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/sh
 set -e
-hugo
 if [[ $(git rev-parse --abbrev-ref HEAD) == master ]]; then
+  hugo
   scp -r public/* root@web01.port-ze.ro:/srv/www/port-zero.com/html
 else
+  hugo --config config.staging.toml
   scp -r public/* root@web01.port-ze.ro:/srv/www/staging.port-zero.com/html
 fi
