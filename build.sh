750fefeead8391ef077c3ef9015fffbeb5489557
hellerve <veit@veitheller.de>
2018-07-31 18:45:26 +0200

    build.sh: clean

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/sh
 set -e
 hugo
-scp -r public/* root@web01.port-ze.ro:/srv/www/port-zero.com/html
+if [[ $(git rev-parse --abbrev-ref HEAD) == master ]]; then
+  scp -r public/* root@web01.port-ze.ro:/srv/www/port-zero.com/html
+else
+  scp -r public/* root@web01.port-ze.ro:/srv/www/staging.port-zero.com/html
+fi
