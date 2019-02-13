3bd5938246b90060cc39c3b0c1d0fda1e75cf9ec
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2019-02-13 12:34:51 +0100

    Fixes prompt for unknown host on CI
    The 'Are you sure you want to continue connecting (yes/no)? ' prompt.

--- scripts/build_and_deploy.sh
+++ scripts/build_and_deploy.sh
@@ -6,12 +6,12 @@ if [[ $CIRCLE_BRANCH == master ]]; then
   echo 'Building for production...'
   hugo
   echo 'Deploying to production...'
-  scp -r public/* root@web01.port-ze.ro:/srv/www/port-zero.com/html
+  scp -o "StrictHostKeyChecking no" -r public/* root@web01.port-ze.ro:/srv/www/port-zero.com/html
 elif [[ $CIRCLE_BRANCH == staging ]]; then
   echo 'Building for staging...'
   hugo --config config.staging.toml
   echo 'Deploying to staging...'
-  scp -r public/* root@web01.port-ze.ro:/srv/www/staging.port-zero.com/html
+  scp -o "StrictHostKeyChecking no" -r public/* root@web01.port-ze.ro:/srv/www/staging.port-zero.com/html
 else
   echo 'Building...'
   hugo
