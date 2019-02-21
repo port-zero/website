0691249e4aac6390c41bbb3a1f7333161f72c0be
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2019-02-21 18:03:09 +0100

    Fixes code style inconsistency

--- themes/portzero/static/js/index.js
+++ themes/portzero/static/js/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-  document.getElementById('language').addEventListener('change', function onChangeLanguage(event) {
+  document.getElementById('language').addEventListener('change', function onChangeLanguage (event) {
     if (window.location.href !== event.target.value) {
       window.location = event.target.value
     }
