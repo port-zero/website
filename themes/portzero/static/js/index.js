2c4d6b738107104e347e05a69db16e2182a5305b
uHGRP <hugurp@protonmail.com>
2019-09-19 21:25:47 +0200

    test a responsive mobile version

--- themes/portzero/static/js/index.js
+++ themes/portzero/static/js/index.js
@@ -1,7 +1,10 @@
-document.addEventListener('DOMContentLoaded', function (event) {
-  document.getElementById('language').addEventListener('change', function onChangeLanguage (event) {
+function onSelectChange (event) {
     if (window.location.href !== event.target.value) {
-      window.location = event.target.value
+	window.location.href = event.target.value
     }
-  })
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+    document.getElementById('language').addEventListener('change', onSelectChange)
+    document.getElementById('mobile-menu').addEventListener('change', onSelectChange)
 })
