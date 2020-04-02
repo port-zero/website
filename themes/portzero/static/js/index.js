c44395db97fa1acfe814e877b0bd1a77782d9995
Tobsic <>
2020-04-02 09:37:03 +0200

    feat(one-page): Create temporary preview for detail tiles on hover.

--- themes/portzero/static/js/index.js
+++ themes/portzero/static/js/index.js
@@ -1,10 +1,31 @@
-function onSelectChange (event) {
-    if (window.location.href !== event.target.value) {
-	window.location.href = event.target.value
-    }
-}
+const detailTilesHover = (event) => {
+  const el = event.target;
+  // Get selection before changing
+  const cbBefore = el.parentNode.querySelector('input:checked');
 
-document.addEventListener('DOMContentLoaded', function (event) {
-    document.getElementById('language').addEventListener('change', onSelectChange)
-    document.getElementById('mobile-menu').addEventListener('change', onSelectChange)
-})
+  // Change selection
+  const cbPreviewId = el.getAttribute('for');
+  const cbPreview = document.getElementById(cbPreviewId);
+  cbPreview.checked = true;
+
+  // restore selection on mouse out, if not clicked in the mean time
+  const onMouseOut = () => {
+    cbPreview.checked = false;
+    if (cbBefore) {
+      cbBefore.checked = true;
+    }
+    removeListeners();
+  };
+  const removeListeners = () => {
+    el.removeEventListener('mouseleave', onMouseOut);
+    el.removeEventListener('click', removeListeners);
+  };
+  el.addEventListener('mouseleave', onMouseOut);
+  el.addEventListener('click', removeListeners)
+};
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  for (const el of document.getElementsByClassName('detail-tiles-preview')) {
+    el.addEventListener('mouseenter', detailTilesHover)
+  }
+});
